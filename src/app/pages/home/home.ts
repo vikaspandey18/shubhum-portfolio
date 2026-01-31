9aeb8505054c2cad173b1f9169d8843cf7f0f252
1996vikas0 <vp2841144@gmail.com>
2026-01-31 13:21:45 +0530

    Upadting the home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-import { Sidebar } from "../../shared/sidebar/sidebar";
-import { Footer } from "../../shared/footer/footer";
+import { Component, signal } from '@angular/core';
+import { Sidebar } from '../../shared/sidebar/sidebar';
+import { Footer } from '../../shared/footer/footer';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
-  imports: [Sidebar, Footer],
+  imports: [Sidebar, Footer, CarouselModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  customOptions: OwlOptions = {
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+  };
+  slidesStore = signal<any[]>([
+    {
+      id: '1',
+      src: '/images/author.jpg',
+      alt: 'Slide 1 HM',
+      dotContent: 'text1',
+    },
+    {
+      id: '2',
+      src: '/images/author-2.jpg',
+      alt: 'Slide 2 HM',
+      dotContent: 'text2',
+    },
+  ]);
+}
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,5 +8,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.scss',
 })
 export class Header {
+
+  @Input() title:string = "";
+  @Input() subTitle:string = "";
 
 }
